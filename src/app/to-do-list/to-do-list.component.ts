175f709ebb1308249af3853957ac8210ecd113eb
Huang-Ming Chang <huang.ming.chang@exact.com>
2018-05-25 09:40:14 +0200

    Use @Input

--- src/app/to-do-list/to-do-list.component.ts
+++ src/app/to-do-list/to-do-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { DataService, ToDoItem } from '../data.service';
 
 @Component({
@@ -7,10 +7,15 @@ import { DataService, ToDoItem } from '../data.service';
   styleUrls: ['./to-do-list.component.css'],
 })
 export class ToDoListComponent implements OnInit {
+  @Input() userName: string = 'John Doe';
   toDoItems: ToDoItem[] = [];
   constructor(private dataService: DataService) {}
 
   ngOnInit() {
     this.toDoItems = this.dataService.toDoItems;
+  }
+
+  getItem({ id, content, status }) {
+    return `${id}-${content}-${status}`;
   }
 }
